8a45af1c76240fa92b9321fac771319db9a8f3cd
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-13 18:51:44 +0500

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import building from '../assets/buildings.png'
+import menu from "../assets/app.png"
 
 const Navbar = () => {
   return (
     <>
       <nav id='navbar'>
         <div className="navbar1">
-            <img src={building} alt="navbar-button" className='navimage' />
+            <img src={building} alt="navbar-button" className='navimage' loading='lazy' />
             <h2 className='taste'>R.Estate</h2>
         </div>
         <ul>
@@ -17,6 +18,7 @@ const Navbar = () => {
             <li>Blog</li>
         </ul>
         <button className='nav-button'>Contact Us</button>
+        <img src={menu} alt="menu-img" className='menu-img' loading='lazy' />
       </nav>
     </>
   )
