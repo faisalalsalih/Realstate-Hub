79f10b18ed4b91d512d016d68ca24dd43806366c
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-16 15:23:35 +0500

    Svg Added to About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import grid from "../assets/grid.svg"
+
 
 const About = () => {
   return (
     <>
-      <section id='section'>
+      <section id='about'>
         <div className="about-container">
-            
+            <img src={grid} alt="grid-svg" className='grid-svg' />
         </div>
       </section>
     </>
