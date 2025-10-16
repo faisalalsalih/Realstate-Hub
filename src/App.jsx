fca48e494b77e551df4e612fe2dadbb7ac817770
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-16 15:10:54 +0500

    About Section Starts from Here

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Cards from './components/Cards'
+import About from './components/About'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cards />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <section id='section'>
+        <div className="about-container">
+            
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default About
