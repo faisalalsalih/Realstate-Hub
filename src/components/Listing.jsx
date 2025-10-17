78261acacd3f1a9924691a9788098c908fe5aeee
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-10-17 18:18:30 +0500

    Card Section Completed

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import home1 from "../assets/homie1.jpeg"
+import home2 from "../assets/homie2.jpeg"
+import home3 from "../assets/homie3.jpeg"
+import heart from "../assets/heart.png"
+import bath from "../assets/bathtub.png"
+import bed from "../assets/bed.png"
+import fourarrows from "../assets/four-arrows.png"
 
 const Listing = () => {
   return (
@@ -11,7 +18,80 @@ const Listing = () => {
             </div>
             <button className='shamatong'>View All Listing</button>
         </div>
-        <div className="listing2"></div>
+        <div className="listing2">
+            {/* SHATO 1 */}
+            <div className="shato1">
+                <img src={home1} alt="shato-img" className='shato-img' />
+                <div className="dollar">
+                    <h6>$95400</h6>
+                    <div className="dill"><img src={heart} alt="heart-image" /></div>
+                </div>
+                <div><h1 className='maison'>Theole sur Mer maison a<br />Vendre</h1></div>
+                <div className="row"></div>
+                <div className="logo-container">
+                    <div className="logo-container1">
+                        <img src={bath} alt="bath-tub" className='bath' />
+                        <p>6 Baths</p>
+                    </div>
+                    <div className="logo-container2">
+                        <img src={bed} alt="rooms" className='bed' />
+                        <p>7 rooms</p>
+                    </div>
+                    <div className="logo-container3">
+                        <img src={fourarrows} alt="rooms" className='arrow' />
+                        <p>1200 Sq</p>
+                    </div>
+                </div>
+            </div>
+            {/* SHATO 2 */}
+            <div className="shato2">
+                <img src={home2} alt="shato-img" className='shato-img' />
+                <div className="dollar">
+                    <h6>$64800</h6>
+                    <div className="dill"><img src={heart} alt="heart-image" /></div>
+                </div>
+                <div><h1 className='maison'>Plonger dans le charm de la<br />Province</h1></div>
+                <div className="row"></div>
+                <div className="logo-container">
+                    <div className="logo-container1">
+                        <img src={bath} alt="bath-tub" className='bath' />
+                        <p>6 Baths</p>
+                    </div>
+                    <div className="logo-container2">
+                        <img src={bed} alt="rooms" className='bed' />
+                        <p>7 rooms</p>
+                    </div>
+                    <div className="logo-container3">
+                        <img src={fourarrows} alt="rooms" className='arrow' />
+                        <p>1200 Sq</p>
+                    </div>
+                </div>
+            </div>
+            {/* SHATO 3 */}
+            <div className="shato3">
+                <img src={home3} alt="shato-img" className='shato-img' />
+                <div className="dollar">
+                    <h6>$85200</h6>
+                    <div className="dill"><img src={heart} alt="heart-image" /></div>
+                </div>
+                <div><h1 className='maison'>Theole sur Mer maison a<br />Vendre</h1></div>
+                <div className="row"></div>
+                <div className="logo-container">
+                    <div className="logo-container1">
+                        <img src={bath} alt="bath-tub" className='bath' />
+                        <p>6 Baths</p>
+                    </div>
+                    <div className="logo-container2">
+                        <img src={bed} alt="rooms" className='bed' />
+                        <p>7 rooms</p>
+                    </div>
+                    <div className="logo-container3">
+                        <img src={fourarrows} alt="rooms" className='arrow' />
+                        <p>1200 Sq</p>
+                    </div>
+                </div>
+            </div>
+        </div>
       </section>
     </>
   )
